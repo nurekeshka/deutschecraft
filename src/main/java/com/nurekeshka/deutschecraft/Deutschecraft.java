38694090a6b3edc892c9639661d202868b18dfae
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-02-16 04:55:14 +0600

    feat: add deutschecraft mod

---
+++ src/main/java/com/nurekeshka/deutschecraft/Deutschecraft.java
@@ -0,0 +1,14 @@
+package com.nurekeshka.deutschecraft;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Deutschecraft.ID)
+public class Deutschecraft {
+    public static final String ID = "deutschecraft";
+
+    public Deutschecraft(IEventBus bus, Dist dist) {
+
+    }
+}
